e1e87168bdc7a14a97df6fa4819e1e3a72e74043
mathieustan <mathieu.stan@gmail.com>
2020-11-17 11:07:08 +0100

    :bug:(Install): Throw if appId isn't properly defined

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,6 +11,8 @@ function mapInstanceToProps (vm, props) {
 }
 
 function isValidType (type, value) {
+  if (!value || value.length === 0) return false;
+
   return value instanceof type ||
     (value !== null && value !== undefined && value.constructor === type);
 }
