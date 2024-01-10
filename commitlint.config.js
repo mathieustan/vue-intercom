443cbc75f04f13876ab8b0411b443ef1ed52db83
Mathieu Stanowski <mathieu.stan@gmail.com>
2024-01-10 08:41:11 +0100

    refactor(chore): commitizen
    Use conventional config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: './commitlint.parser.js',
-  rules: {
-    /*
-    * types can be found in .cz-config.js
-    */
-    'type-enum': [2, 'always', [
-      ':sparkles:',
-      ':bug:',
-      ':pencil:',
-      ':lipstick:',
-      ':art:',
-      ':zap:',
-      ':white_check_mark:',
-      ':package:',
-      ':rewind:',
-      ':tata:',
-    ]],
-    'scope-empty': [
-      2,
-      'never',
-    ],
-  },
 };
